685a1b5d9d62eee4b585a4d3722ef6de982ca878
zakka031 <zakaria.el.bakkali.2006@gmail.com>
2024-09-16 11:57:59 +0200

    Your commit message

--- resources/views/about.blade.php
+++ resources/views/about.blade.php
@@ -1,17 +1,9 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
-</head>
-<body>
-<nav>
-<a href="home">home</a>
-<a href="about">about</a>
-<a href="contact">contact</a>
-</nav>
+<x-layout>
+ <x-slot:heading>
+    about Page
+</x-slot:heading>
+<h1>Hello from the about page</h1>
 
-    <h1>Hello from the about page</h1>
-</body>
-</html>
+
+</x-layout>
+
